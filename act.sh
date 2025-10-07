cc901a2295a0ab91d945cedd01b76c8f32cdbe7a
sma9000 <sma9000@nyu.edu>
2025-10-07 18:46:57 -0400

    Added comment to act.sh explaining how to acti vate virtual environment

--- act.sh
+++ act.sh
@@ -1,2 +1,4 @@
 #/bin/bash
+# This script activates the project's Python virtual environment 
+# so that all Python commands use the correct dependencies and versions.
 source min-demo-venv/bin/activate
