92d03fdfaf15f81941e0de8cdbeb776192ac937c
Nina Li <nl2539@nyu.edu>
2025-10-07 19:53:35 -0400

    added create function for cities

---
+++ cities/queries.py
@@ -0,0 +1,12 @@
+city_cache = {
+    
+}
+
+def create(data, dict):
+    if not isinstance(data, dict):
+        raise ValueError(f'Bad type for {type(data)=}')
+    if not data.get(NAME):
+        raise ValueError(f'Bad type for {data.get(NAME)=}')
+    new_id = str(len(city_cache) + 1)
+    city_cache[new_id] = data
+    return new_id
