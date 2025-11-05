91c0c2ebb0bf7e3089e6df0fe88c2ade15e6fb3b
sma9000 <sma9000@nyu.edu>
2025-11-04 20:25:15 -0500

    add basic unit tests for country query functions

---
+++ countries/tests/test_country_queries.py
@@ -0,0 +1,17 @@
+import pytest
+from countries.queries import get_country, num_countries, read_all
+
+
+def test_get_country_valid():
+    country = get_country(1)
+    assert country["name"] == "United States"
+    assert country["capital"] == "Washington, DC"
+
+
+def test_get_country_invalid():
+    with pytest.raises(ValueError):
+        get_country(999)
+
+
+def test_num_countries():
+    assert num_countries() == len(read_all())
