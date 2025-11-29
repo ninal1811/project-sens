e93a9624b1a615d72c860f28ffa27224ad9cfd9a
suha-hoque <sh6942@nyu.edu>
2025-11-29 10:20:05 -0500

    commit for Connect to MongoDB in the Cloud assignment

--- data/db_connect.py
+++ data/db_connect.py
@@ -74,9 +74,7 @@ def connect_db():
                 raise ValueError('You must set your password '
                                  + 'to use Mongo in the cloud.')
             print('Connecting to Mongo in the cloud.')
-            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
-                                    + '@koukoumongo1.yud9b.mongodb.net/'
-                                    + '?retryWrites=true&w=majority')
+            client = pm.MongoClient(f"mongodb+srv://suha_hoque:{password}@cluster0.dsx6edq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         else:
             print("Connecting to Mongo locally.")
             client = pm.MongoClient()
