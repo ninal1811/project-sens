a580f291d07236a595f28b48e8e2219418ebc40a
Erica Belcena <ericabelcena@gmail.com>
2025-12-03 14:18:37 -0500

    added extract, transform, and load to load_states_lat_long.py

--- states/ETL/load_states_lat_long.py
+++ states/ETL/load_states_lat_long.py
@@ -8,6 +8,25 @@ from states.queries import (
 
 CURR_COUNTRY = 'USA'
 
+def extract(flnm: str) -> list:
+    state_list = []
+
+def transform(state_list: list) -> list:
+    rev_list = []
+    col_names = state_list.pop(0)
+    for state in state_list:
+        state_dict = {}
+        for i, fld in enumerate(col_names):
+            state_dict[fld] = state[i]
+        state_dict[COUNTRY_CODE] = CURR_COUNTRY
+        rev_list.append(state_dict)
+    return rev_list
+
+
+def load(rev_list: list):
+    for state in rev_list:
+        create(state, reload=False)
+
 def main():
     exit(1)
     state_list = extract(sys.argv[1])
