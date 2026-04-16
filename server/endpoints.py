4f31bd8d98dcac325e10969d428eb20ed4666cd3
ChrisCortesSanchez <cc7825@nyu.edu>
2026-04-15 22:47:07 -0400

    Set session_cookie_secure to true so that cross origin cookies works properly

--- server/endpoints.py
+++ server/endpoints.py
@@ -47,7 +47,7 @@ VERSION_NAME = "project-sens"
 
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
-app.config['SESSION_COOKIE_SECURE'] = False  # Set True in production with HTTPS
+app.config['SESSION_COOKIE_SECURE'] = True  # Set True in production with HTTPS
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 
